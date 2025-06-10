7de97f568b467585449e3a69056b534e5b82e725
psaegert <paulsaegert@protonmail.com>
2025-06-10 23:59:16 +0200

    Pre-compute all hashes of up to max size to guarantee rule finding

--- src/simplipy/__main__.py
+++ src/simplipy/__main__.py
@@ -3,15 +3,15 @@ import sys
 
 
 def main(argv: str = None) -> None:
-    parser = argparse.ArgumentParser(description='Neural Symbolic Regression')
+    parser = argparse.ArgumentParser(description='SimpliPy CLI Tool')
     subparsers = parser.add_subparsers(dest='command_name', required=True)
 
-    find_simplifications_parser = subparsers.add_parser("find-simplifications")
-    find_simplifications_parser.add_argument('-e', '--expression-space', type=str, required=True, help='Path to the expression space configuration file')
+    find_simplifications_parser = subparsers.add_parser("find-rules")
+    find_simplifications_parser.add_argument('-e', '--engine', type=str, required=True, help='Path to the engine configuration file')
+    find_simplifications_parser.add_argument('-g', '--config', type=str, required=True, help='Path to the rule-finding configuration file')
     find_simplifications_parser.add_argument('-n', '--max_n_rules', type=int, default=None, help='Maximum number of rules to find')
     find_simplifications_parser.add_argument('-l', '--max_pattern_length', type=int, default=7, help='Maximum length of the patterns to find')
     find_simplifications_parser.add_argument('-t', '--timeout', type=int, default=None, help='Timeout for the search of simplifications in seconds')
-    find_simplifications_parser.add_argument('-d', '--dummy-variables', type=int, nargs='+', default=None, help='Dummy variables to use in the simplifications')
     find_simplifications_parser.add_argument('-m', '--max-simplify-steps', type=int, default=5, help='Maximum number of simplification steps')
     find_simplifications_parser.add_argument('-x', '--X', type=int, default=1024, help='Number of samples to use for comparison of images')
     find_simplifications_parser.add_argument('-c', '--C', type=int, default=1024, help='Number of samples of constants to put in to placeholders')
@@ -26,26 +26,28 @@ def main(argv: str = None) -> None:
 
     # Execute the command
     match args.command_name:
-        case 'find-simplifications':
+        case 'find-rules':
             if args.verbose:
-                print(f'Finding simplifications with expression space {args.expression_space}')
+                print(f'Finding simplifications with engine {args.engine}')
             import os
             from simplipy import SimpliPyEngine
-            from simplipy.utils import substitute_root_path
+            from simplipy.utils import substitute_root_path, load_config
 
-            expression_space = SimpliPyEngine.from_config(substitute_root_path(args.expression_space))
+            engine = SimpliPyEngine.from_config(substitute_root_path(args.engine))
 
             resolved_output_file = substitute_root_path(args.output_file)
 
             if not os.path.exists(os.path.dirname(resolved_output_file)):
                 os.makedirs(os.path.dirname(resolved_output_file), exist_ok=True)
 
-            expression_space.find_rules(
+            rule_finding_config = load_config(substitute_root_path(args.config), resolve_paths=True)
+
+            engine.find_rules(
                 max_n_rules=args.max_n_rules,
                 max_pattern_length=args.max_pattern_length,
                 timeout=args.timeout,
-                dummy_variables=args.dummy_variables,
-                max_simplify_steps=args.max_simplify_steps,
+                dummy_variables=rule_finding_config.get('dummy_variables', None),
+                extra_internal_terms=rule_finding_config.get('extra_internal_terms', None),
                 X=args.X,
                 C=args.C,
                 constants_fit_retries=args.constants_fit_retries,
